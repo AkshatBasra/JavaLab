fddd4aae884a7a0d2e9cda93958c17f8e77896b2
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2024-10-30 12:51:22 +0530

    Add files via upload

--- Record/Bank.java
+++ Record/Bank.java
@@ -1,25 +1,103 @@
-public class Account {
-    int accountNumber, balance;
-    Account(int accountNumber){
-        this.accountNumber = accountNumber;
-        this.balance = 0;
-    }
-    void deposit(int depAmount){
-        balance += depAmount;
-    }
-    void withdraw(int withdrawAmount){
-        balance -= withdrawAmount;
-    }
+import java.util.Scanner;
+import java.lang.Math;
 
-    public static void main(String[] args) {
-        System.out.println("Akshat Basra 1BM23CS020");
-        Account[] accounts = new Account[15];
-        for (int i = 0; i < 15; i++) {
-            accounts[i] = new Account(i+1);
-            accounts[i].deposit(i*500);
-            accounts[i].withdraw(i);
-            System.out.print(accounts[i].accountNumber+" : ");
-            System.out.println(accounts[i].balance);
-        }
-    }
+class Bank{
+	int accountNo;
+	double balance;
+	Bank(int accountNo){
+		this.accountNo = accountNo;
+		this.balance = 0;
+	}
+	void deposit(double depAmount){
+		this.balance += depAmount;
+	}
+	void withdraw(double withAmount){
+		this.balance -= withAmount;
+	}
 }
+
+
+class SavingsAccount extends Bank{
+	double interest;
+	SavingsAccount(int accountNo){
+		super(accountNo);
+	}
+/*	void deposit(double depAmount){
+		super.balance += depAmount;
+	}
+	void withdraw(double withAmount){
+		super.balance -= withAmount;
+	}*/
+	double interest(double rate, int time){
+		interest = balance * Math.pow((1 + rate / 100), time);
+		balance += interest;
+		return interest;
+	}
+}
+
+class CurrentAccount extends Bank{
+	static double withdrawLimit = 1000;
+	CurrentAccount(int accountNo){
+		super(accountNo);
+	}
+/*	void deposit(double depAmount){
+		super.balance += depAmount;
+	}*/
+	public void withdraw(double withAmount){
+		super.balance -= withAmount;
+		if(balance < withdrawLimit){
+			System.out.println("Withdraw Limit Reached - Deducting Service Charge");
+			balance -= 100;
+		}
+	double interest(){
+		System.out.println("Interest is not applicable in current account");
+		return 0.0;
+	}
+}
+
+
+class Run{
+	public static void main(String[] args){
+		System.out.println("Akshat Basra 1BM23CS020");
+		double amount;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("1. Open Savings Account\n2. Open Current Account\n\nEnter Choice:");
+		int choice = sc.nextInt()
+		if(choice == 1)
+			SavingsAccount acc = new SavingsAccount(101);
+		else
+			CurrentAccount acc = new CurrentAccount(201);
+		System.out.println("1. Deposit\n2. Withdraw\n3. Show Balance\n4. Compute Interest\n5. Exit\n");
+		while(1){
+			choice = sc.nextInt();
+			switch(choice){
+				case 1: System.out.print("Enter deposit amount: ");
+					amount = nextDouble();
+					acc.deposit(amount);
+					break;
+				case 2: System.out.print("Enter withdraw amount: ");
+					amount = nextDouble();
+					acc.withdraw(amount);
+					break;
+				case 3: System.out.println("The balance is " + acc.balance);
+					break;
+				case 4: System.out.println("The interest is " + acc.interest());
+					break;
+				default:exit(0);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
