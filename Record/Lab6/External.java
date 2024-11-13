a8639db690a0ea32486f2982931a8486eb3e0907
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2024-11-13 21:39:22 +0530

    Update External.java

--- Record/Lab6/External.java
+++ Record/Lab6/External.java
@@ -1,18 +1,10 @@
-package SEE; 
-import CIE.Student; 
-public class External extends Student 
-{ private int[] externalMarks;  
-public External(String name, int[] externalMarks) 
-{ 
-super(name); 
-this.externalMarks = externalMarks; 
-this.setMarks(externalMarks);
-} 
-public int[] getExternalMarks() 
-{ 
-return externalMarks; 
-} 
-public void setExternalMarks(int[] externalMarks) 
-{ this.externalMarks = externalMarks; 
-this.setMarks(externalMarks); }
- }
+package SEE;
+import CIE.Student;
+public class External extends Student {
+    int[] externalMarks;
+    public External(String name, int[] externalMarks) {
+        super(name);
+        this.externalMarks = externalMarks;
+        this.setMarks(externalMarks);
+    }
+}
