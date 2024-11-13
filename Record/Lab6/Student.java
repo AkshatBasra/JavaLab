733e6c6aef7ab9ab42e2939aef8f7e85058d8f24
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2024-11-13 21:40:46 +0530

    Update Student.java

--- Record/Lab6/Student.java
+++ Record/Lab6/Student.java
@@ -1,22 +1,18 @@
-
- package CIE; 
-public class Student { 
-protected String name; 
-protected int[] marks; 
-public Student(String name) 
-{ 
-this.name = name; 
-this.marks = new int[5];
- }
- public String getName() 
-{ 
-return name; 
-} 
-public void setMarks(int[] marks) 
-{ 
-this.marks = marks; 
-}
- public int[] getMarks() { 
-return marks; 
-} 
+package CIE;
+public class Student {
+    protected String name;
+    protected int[] marks;
+    public Student(String name) {
+        this.name = name;
+        this.marks = new int[5];
+    }
+    public String getName() {
+        return name;
+    }
+    public void setMarks(int[] marks) {
+        this.marks = marks;
+    }
+    public int[] getMarks() {
+        return marks;
+    }
 }
